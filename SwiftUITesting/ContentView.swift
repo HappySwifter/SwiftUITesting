0e3217565e707cb921e9bb4234349d5829e83cfb
Artem <artem.valiev@gmail.com>
2020-03-01 10:24:55 +0300

    Initial Commit

---
+++ SwiftUITesting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITesting
+//
+//  Created by Артем Валиев on 01.03.2020.
+//  Copyright © 2020 Артем Валиев. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
